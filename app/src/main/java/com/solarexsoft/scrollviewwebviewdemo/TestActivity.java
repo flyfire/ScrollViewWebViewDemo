51f7fa281465b7233b5aefa08815a805a4dce47f
Solarex <rh.hou.work@gmail.com>
2018-04-25 16:47:54 +0800

    test

--- app/src/main/java/com/solarexsoft/scrollviewwebviewdemo/TestActivity.java
+++ app/src/main/java/com/solarexsoft/scrollviewwebviewdemo/TestActivity.java
@@ -32,10 +32,21 @@ public class TestActivity extends AppCompatActivity {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                String js = "var appendDiv = document.createElement('div');document.body.prepend(appendDiv);appendDiv.innerHTML='<div><img src=\"http://pub-1251962406.file.myqcloud.com/skylight/resources/4c0475ed-0816-449b-8569-d7e02164307a.png\" width=\"100%%\" height=\"auto\"></div>';";
-//                String js = "var appenddiv = document.createElement('div');appenddiv.innerHTML='<div style=\"width:100px; height:100px; background-color:#666; z-index:1\"></div>'; document.body.prepend(appenddiv);";
-                view.loadUrl("javascript:" + js);
-                iv.animate().alpha(0).setStartDelay(2000).setDuration(200).start();
+//                String js = "var appendDiv = document.createElement('div');document.body
+// .prepend(appendDiv);appendDiv.innerHTML='<div><img src=\"http://pub-1251962406.file.myqcloud
+// .com/skylight/resources/4c0475ed-0816-449b-8569-d7e02164307a.png\" width=\"100%%\"
+// height=\"auto\"></div>';";
+//                String js = "var appendDiv = document.createElement('div');document.body
+// .innerHTML='<div><img src=\"http://pub-1251962406.file.myqcloud
+// .com/skylight/resources/4c0475ed-0816-449b-8569-d7e02164307a.png\" width=\"100px\"
+// height=\"100px\"></div>'; console.log('solarex');";
+//                String js = "var appenddiv = document.createElement('div');appenddiv
+// .innerHTML='<div style=\"width:100px; height:100px; background-color:#666;
+// z-index:1\"></div>'; document.body.prepend(appenddiv);";
+//                String js = "document.body.innerHTML= '<div><img src=\"http://pub-1251962406.file.myqcloud.com/skylight/resources/4c0475ed-0816-449b-8569-d7e02164307a.png\"></div>'";
+                String js = "document.body.innerHTML = '<img src=\"https://icxpic-10023060.file.myqcloud.com/items/7df0382404a74132961ef82f3acbf5ea.jpeg\">'";
+                view.loadUrl("javascript: " + js);
+                iv.animate().alpha(0).setStartDelay(100).setDuration(200).start();
             }
         });
         WebSettings webSettings = webView.getSettings();
@@ -48,6 +59,7 @@ public class TestActivity extends AppCompatActivity {
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setBlockNetworkImage(false);
 //        webView.loadUrl("https://main.icarbonx.com/mallMicro/2000126");
-        webView.loadUrl("http://www.baidu.com");
+//        webView.loadUrl("http://www.baidu.com");
+        webView.loadUrl("http://www.qq.com");
     }
 }
